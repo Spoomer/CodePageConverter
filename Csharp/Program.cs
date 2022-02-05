d3314d3002db32533f01d5ee475625da2bec070b
Spoomer <renerahnfeld@gmail.com>
2022-02-05 21:09:39 +0100

    encoding fix latin1 = win-1252

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -18,23 +18,21 @@ public class Program
             inputFile = args[0];
         }
         Console.WriteLine(inputFile);
-        Console.WriteLine("Which Encoding has the Inputfile?  (1) UTF8 (2) Latin1/ISO-8859-1 (3) Windows-1252 Western Europe");
+        Console.WriteLine("Which Encoding has the Inputfile?  (1) UTF8 (2) Latin1/ISO-8859-1/Windows-1252 Western Europe");
         string? readLine = Console.ReadLine();
         Encoding inputEncoding = readLine switch
         {
             "1" => Encoding.UTF8,
             "2" => Encoding.Latin1,
-            "3" => Encoding.GetEncoding(1252),
             _ => throw new Exception("supports only UTF8, Latin1/ISO-8859-1 or Windows-1252"),
         };
-        Console.WriteLine("To which Encoding should it be converted? (1) UTF8 (2) Latin1/ISO-8859-1 (3) Windows-1252 Western Europe");
+        Console.WriteLine("To which Encoding should it be converted? (1) UTF8 (2) Latin1/ISO-8859-1/Windows-1252 Western Europe");
         string? readLineOutput = Console.ReadLine();
         Encoding outputEncoding = readLineOutput switch
         {
             "1" => Encoding.UTF8,
             "2" => Encoding.Latin1,
-            "3" => Encoding.GetEncoding(1252),
-            _ => throw new Exception("supports only UTF8, Latin1/ISO-8859-1 or Windows-1252"),
+            _ => throw new Exception("supports only UTF8, Latin1/ISO-8859-1/Windows-1252"),
         };
         FileInfo file = new(inputFile);
         var fullNameWithoutExtension = file.FullName[..file.FullName.LastIndexOf('.')].Replace("_"+inputEncoding.HeaderName,"");
